8a82805ce3f54e5a77078609c9ef69ec029b897c
Yusuf <40708511+indonesia08@users.noreply.github.com>
2019-09-24 23:12:56 +0700

    Create Mobil.java

---
+++ Pertemuan1/Mobil.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Pertemuan1;
+
+/**
+ *
+ * @author yusuf-laptop
+ */
+public class Mobil {
+    String warna="hitam";
+    int tahun=2015;
+}
