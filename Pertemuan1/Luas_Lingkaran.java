bcdf0ce6b29ed42d055895c0b2ee7297b97688a1
Yusuf <40708511+indonesia08@users.noreply.github.com>
2019-09-24 23:12:32 +0700

    Create Luas_Lingkaran.java

---
+++ Pertemuan1/Luas_Lingkaran.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Pertemuan1;
+
+/**
+ *
+ * @author yusuf-laptop
+ */
+public class Luas_Lingkaran {
+    public static void main(String[] args) {
+        final double phi =3.14;
+        
+        int jari2 =10;
+        double lk = phi*jari2*jari2;
+
+        System.out.println("Luas Lingkaran\n");
+        System.out.println("Phi: "+phi);
+        System.out.println("Jari-jari : "+jari2);
+        System.out.println("Hasil luas :"+lk);
+    }
+}
