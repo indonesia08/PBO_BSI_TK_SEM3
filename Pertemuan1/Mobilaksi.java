86343349d9af3f38c7bde35928a779a8aab748ef
Yusuf <40708511+indonesia08@users.noreply.github.com>
2019-09-24 23:13:19 +0700

    Create Mobilaksi.java

---
+++ Pertemuan1/Mobilaksi.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Pertemuan1;
+
+/**
+ *
+ * @author yusuf-laptop
+ */
+public class Mobilaksi {
+    public static void main(String[] args) {
+        //instalasi object
+        Mobil obj=new Mobil();
+        //pernyataan cetak
+        System.out.println("Konsep dasar OOP");
+        System.out.println("Warna:"+obj.warna);
+        System.out.println("Tahun:"+obj.tahun);
+    }
+}
